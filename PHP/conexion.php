90ca66f7bdbe089904061feb1d35ad8ca6900c29
Lukitas <75153216+Nad-hue@users.noreply.github.com>
2022-10-27 17:04:05 -0300

    Conexion PDO
    Cambie la conexion para que sea tipo PDO embez de la clasica en Mysqli

--- PHP/conexion.php
+++ PHP/conexion.php
@@ -1,4 +1,18 @@
 <?php
-    $conexion = new mysqli("sql310.epizy.com","epiz_32458715","5t3lnJutj8qMqoZ","epiz_32458715_base_rojas")
+    $nombreServidor ="localhost";
+    $db = "base_rojas";
+    $usuario = "root";
+    $contraseña = "";
 
+    try {
+        $conexion = new PDO("mysql:host=$nombreServidor;dbname=$db", $usuario,$contraseña);
+        $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        echo "conexion exitosa";
+        return $conexion;
+
+    }catch (PDOException $e ){
+        echo "Error en la conexion: " .$e->getMessage();
+        echo "Linea: ".$e->getLine();
+    }
 ?>
--- vistas/Turnos/insert.php
+++ vistas/Turnos/insert.php
@@ -1,5 +1,4 @@
 <?php
 
-$name= $_POST['name']
-
+include("../../PHP/conexion.php");
 ?>
